5d28792cd4dd4edff267aecc62eab80435213f58
Victor Jozwicki <tigre.demon@hotmail.fr>
2016-01-29 20:26:40 +0100

    Initial commit
    Initial commit with implementation of all classes

---
+++ GameScreen.java
@@ -0,0 +1,66 @@
+package com.mygdx.game;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public class GameScreen implements Screen {
+	SpriteBatch batch = new SpriteBatch();
+	Texture block = TextureFactory.getInstance().getBlockTexture();
+	Texture img2 = TextureFactory.getInstance().getPacmanTexture();
+	
+	GameScreen() {
+		// Nuthing
+	}
+	
+	@Override
+	public void show() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void render(float delta) {
+		Gdx.gl.glClearColor(1, 1, 1, 1);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		batch.begin();
+
+		batch.draw(block, 200, 5); //temp
+		batch.draw(img2, 250, 20); //temp
+		batch.end();
+		
+	}
+
+	@Override
+	public void resize(int width, int height) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void pause() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void resume() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void hide() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void dispose() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
